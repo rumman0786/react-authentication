16cf174726905f1062fff8436b3c5709f3bfd645
Rumman Ashraf <rumman.ashraf@gmail.com>
2021-05-07 16:38:07 +0600

    Added change passoword feature

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,38 @@
 import classes from './ProfileForm.module.css';
+import { useRef, useContext } from 'react';
+
+import AuthContext from '../../store/auth-context';
 
 const ProfileForm = () => {
+  const newPasswordRef = useRef();
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const changedPassword = newPasswordRef.current.value;
+
+    fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyD1s7srmeAr57Qb-XxhuAtKeRhQo3psSEI', {
+      method: 'POST',
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: changedPassword,
+        returnSecureToken: false
+    }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then( res => console.log(res) 
+      
+    );
+  }
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength='6' ref={newPasswordRef}/>
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
